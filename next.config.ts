4221feb357a7429c5a6522ab9b0372947b62cd24
Ruan Lopes <intel.spec.lopes@gmail.com>
2026-02-27 15:27:42 -0400

    mural

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mural.web.fslab.dev',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.fslab.dev',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
